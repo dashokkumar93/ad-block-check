3b1ac2fead0fd0ef8e9802030cfeead0eb823537
Ashok kumar <ashokkumar01cbe@gmail.com>
2016-05-24 19:57:16 +0530

    Update adframe.js

--- adframe.js
+++ adframe.js
@@ -1,13 +1,2 @@
 isBlocked=false;
 console.log("Its Executin");
- var request = new XMLHttpRequest();
-  request.onreadystatechange = function() {
-    if(request.readyState === 4 && request.status === 200 ) {
-      console.log('No blocker');
-    }
-    else if(request.readyState === 4 && request.status === 0){
-      console.log('Blocker exists');
-    }
-  };
-  request.open("GET","pathTo/ads.html");
-  request.send();
