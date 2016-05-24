b2c1fbbe8389db30a8584a7abed3b2312f6ba691
Ashok kumar <ashokkumar01cbe@gmail.com>
2016-05-24 20:24:25 +0530

    Update adframe.js

--- adframe.js
+++ adframe.js
@@ -1,3 +1,1 @@
 isBlocked=false;
-console.log("Its Executin");
-var a='<ins class="adsbygoogle" style="display:inline-block;width:300px;height:250px"     data-ad-client="ca-pub-4623469134243566"     data-ad-slot="7341487737"></ins>';
